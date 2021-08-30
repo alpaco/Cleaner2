fe3e7f58f46eb8cb268ba45128f5db61b9d8f395
incarnadine24 <wenq.zz123@gmail.com>
2021-08-30 15:22:42 +0300

    batterysaver ok

--- app/src/main/java/com/missclickads/cleaner/ui/batterysaver/BatterySaverFragment.kt
+++ app/src/main/java/com/missclickads/cleaner/ui/batterysaver/BatterySaverFragment.kt
@@ -1,5 +1,6 @@
 package com.missclickads.cleaner.ui.batterysaver
 
+import android.annotation.SuppressLint
 import android.content.Context.BATTERY_SERVICE
 import android.os.BatteryManager
 import android.os.Bundle
@@ -7,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -30,18 +32,36 @@ class BatterySaverFragment : Fragment() {
         return root
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val bm = (activity as MainActivity).getSystemService(BATTERY_SERVICE) as BatteryManager
         val batteryInfo = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
         val textbattery = view.findViewById<TextView>(R.id.text_process)
+        val textTime = view.findViewById<TextView>(R.id.text_time)
         val btnOptimize = view.findViewById<Button>(R.id.btn_optimize)
+        val imageOk = view.findViewById<ImageView>(R.id.image_ok)
+        val imageCircle = view.findViewById<ImageView>(R.id.imageView)
+        val textResult = view.findViewById<TextView>(R.id.text_process)
         textbattery.text ="$batteryInfo %"
-        btnOptimize.setOnClickListener {
-            println("loh")
-            println(batteryInfo)
 
+        //before optimize
+        val batteryhours = batteryInfo * 4 /60
+        val batteryminutes = batteryInfo *4 % 60
+
+        //after optimize
+
+        val batteryhoursafter = (batteryInfo * 1.2 * 4 / 60).toInt()
+        val batteryminutesafter = (batteryInfo * 1.2 * 4 % 60).toInt()
+
+
+        textTime.text="$batteryhours h $batteryminutes m"
+        btnOptimize.setOnClickListener {
+            textTime.text = "$batteryhoursafter h $batteryminutesafter m"
+            textResult.visibility = View.INVISIBLE
+            imageOk.visibility = View.VISIBLE
+            imageCircle.setImageResource(R.drawable.ellipse_blue)
         }
 
     }
