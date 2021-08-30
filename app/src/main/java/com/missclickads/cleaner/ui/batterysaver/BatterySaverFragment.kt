10155e13606b169947704027129a1cd7ba0f5ef8
incarnadine24 <wenq.zz123@gmail.com>
2021-08-30 14:23:18 +0300

    battery

--- app/src/main/java/com/missclickads/cleaner/ui/batterysaver/BatterySaverFragment.kt
+++ app/src/main/java/com/missclickads/cleaner/ui/batterysaver/BatterySaverFragment.kt
@@ -1,5 +1,7 @@
 package com.missclickads.cleaner.ui.batterysaver
 
+import android.content.Context.BATTERY_SERVICE
+import android.os.BatteryManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.missclickads.cleaner.MainActivity
 import com.missclickads.cleaner.R
 
 class BatterySaverFragment : Fragment() {
@@ -26,3 +29,15 @@ class BatterySaverFragment : Fragment() {
         return root
     }
 }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bm = (activity as MainActivity).getSystemService(BATTERY_SERVICE) as BatteryManager
+        val batteryInfo = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+
+        btnOptimize.setOnClickListener {
+            println(batteryInfo)
+        }
+
+    }
