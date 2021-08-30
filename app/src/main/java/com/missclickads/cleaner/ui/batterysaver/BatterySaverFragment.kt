3e7c58fb21e3edd68d8ce51b5a2d336296f03f45
incarnadine24 <wenq.zz123@gmail.com>
2021-08-30 23:07:36 +0300

    optimizer, colourText on battery

--- app/src/main/java/com/missclickads/cleaner/ui/batterysaver/BatterySaverFragment.kt
+++ app/src/main/java/com/missclickads/cleaner/ui/batterysaver/BatterySaverFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -62,6 +63,7 @@ class BatterySaverFragment : Fragment() {
             textResult.visibility = View.INVISIBLE
             imageOk.visibility = View.VISIBLE
             imageCircle.setImageResource(R.drawable.ellipse_blue)
+            textTime.setTextColor(ContextCompat.getColor((activity as MainActivity), R.color.gradient_blue_start))
         }
 
     }
--- app/src/main/java/com/missclickads/cleaner/ui/optimizer/OptimizerFragment.kt
+++ app/src/main/java/com/missclickads/cleaner/ui/optimizer/OptimizerFragment.kt
@@ -1,13 +1,18 @@
 package com.missclickads.cleaner.ui.optimizer
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.missclickads.cleaner.MainActivity
 import com.missclickads.cleaner.R
 
 class OptimizerFragment : Fragment() {
@@ -25,4 +30,28 @@ class OptimizerFragment : Fragment() {
 
         return root
     }
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val textResult = view.findViewById<TextView>(R.id.text_process)
+        val imageCircle = view.findViewById<ImageView>(R.id.imageView)
+        val btnOptimize = view.findViewById<Button>(R.id.btn_optimize)
+
+        //before optimize
+        val temp = (40..50).random()
+        //after optimize
+        val tempAfter = (temp * 0.85).toInt()
+
+
+        textResult.text = "$temp °C"
+        btnOptimize.setOnClickListener {
+            textResult.text = "$tempAfter °C"
+            imageCircle.setImageResource(R.drawable.ellipse_blue)
+            textResult.setTextColor(ContextCompat.getColor((activity as MainActivity), R.color.gradient_blue_start))
+
+        }
+    }
+
 }
