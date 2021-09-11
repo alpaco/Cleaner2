8a7cd557c7855dba10ff4afa259278c0137b4a1d
daniloday <danil.vladyka1@gmail.com>
2021-09-11 15:06:22 +0300

    splash

--- app/src/main/java/com/missclickads/cleaner/SplashActivity.kt
+++ app/src/main/java/com/missclickads/cleaner/SplashActivity.kt
@@ -1,6 +1,7 @@
 package com.missclickads.cleaner
 
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.graphics.LinearGradient
@@ -15,24 +16,21 @@ import androidx.core.content.ContextCompat
 
 class SplashActivity : AppCompatActivity() {
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_splash)
         val progressBar = findViewById<ProgressBar>(R.id.progressBarSplash)
-//        progressBar.progressDrawable = resources.getDrawable(R.drawable.progress_bar_hor_blue)
         val animation = ObjectAnimator.ofInt(progressBar, "progress", 0, 100)
-         animation.duration = (8.5 * 1000).toLong()
-    }
-
-
-
-    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
-
+        animation.duration = (8.5 * 1000).toLong()
+        animation.start()
         Handler().postDelayed({
             val intent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(intent)
         }, (8.5 * 1000).toLong())
-        return super.onCreateView(name, context, attrs)
     }
+
+
+
 }
