9dfd0e9714c9413076894c263bac796def9289b6
incarnadine24 <wenq.zz123@gmail.com>
2021-09-13 22:14:05 +0300

    gradientResultFragment

--- app/src/main/java/com/missclickads/cleaner/ui/result/ResultFragment.kt
+++ app/src/main/java/com/missclickads/cleaner/ui/result/ResultFragment.kt
@@ -1,11 +1,16 @@
 package com.missclickads.cleaner.ui.result
 
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.missclickads.cleaner.MainActivity
 import com.missclickads.cleaner.R
 
 class ResultFragment : Fragment() {
@@ -19,6 +24,18 @@ class ResultFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.result_fragment, container, false)
+
+        val textCount = view.findViewById<TextView>(R.id.textCountOptimized)
+
+        val paint = textCount.paint
+        val width = paint.measureText(textCount.text.toString())
+        val textShader: Shader = LinearGradient(0f, 0f, width, textCount.textSize, intArrayOf(
+            ContextCompat.getColor((activity as MainActivity), R.color.gradient_blue_end) ,
+            ContextCompat.getColor((activity as MainActivity), R.color.gradient_blue_middle) ,
+            ContextCompat.getColor((activity as MainActivity), R.color.gradient_blue_start)
+        ), null, Shader.TileMode.CLAMP)
+        textCount.paint.setShader(textShader)
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
